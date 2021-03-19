ae120e27cc0a31400e4549794e7e7ae8fd99875c
Lenin <lenincom2017@gmail.com>
2021-03-19 16:33:55 +0530

    validation

--- assets/js/main.js
+++ assets/js/main.js
@@ -57,6 +57,8 @@ sr.reveal('.work__img',{interval: 200});
 sr.reveal('.contact__input',{interval: 200}); 
 
 
+
+
 document.getElementById("goResume").onclick = function () {
     location.href = "./portfolio/index.html";
 };
